a2bc23b322901d150ad33cc005668a8c549675bf
diegomagalhaes-dev <digengup@gmail.com>
2022-03-17 23:11:59 -0300

    :sparkles: create custom_user and user_authentication

--- appPetShop/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from appPetShop.models import Endereco, Cliente, Animal, Servico
-
-admin.site.register(Endereco)
-admin.site.register(Cliente)
-admin.site.register(Animal)
-admin.site.register(Servico)
-
